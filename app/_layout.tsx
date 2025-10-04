f81b8d40fefce9b70ca9760c2b091e8b9ee4458d
Ananya-CMTAI <ananya.namdev@completrix.com>
2025-10-04 23:53:04 +0530

    do

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,13 @@
 // app/_layout.tsx
-import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-// import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 import { Provider } from 'react-redux';
-import { store } from '../redux/store'; // Adjust path if needed
+import { store } from '../redux/store';
 
 export default function RootLayout() {
-  // useFrameworkReady();
-
   return (
     <Provider store={store}>
-      <Stack screenOptions={{ headerShown: false }}>
+      <Stack initialRouteName="signuploginscreen" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="signuploginscreen" />
       </Stack>
       <StatusBar style="auto" />
