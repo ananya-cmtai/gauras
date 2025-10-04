3202e2af9045b7b1cea048cddd2421be4fedc5e0
Ananya-CMTAI <ananya.namdev@completrix.com>
2025-10-05 00:37:29 +0530

    do

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,14 @@
 // app/_layout.tsx
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { Provider } from 'react-redux';
-import { store } from '../redux/store';
+import SignupLoginScreen from './signuploginscreen';
 
 export default function RootLayout() {
   return (
-    <Provider store={store}>
-      <Stack initialRouteName="signuploginscreen" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="signuploginscreen" />
-      </Stack>
-      <StatusBar style="auto" />
-    </Provider>
+    // <Provider store={store}>
+    //   <Stack initialRouteName="signuploginscreen" screenOptions={{ headerShown: false }}>
+    //     <Stack.Screen name="signuploginscreen" />
+    //   </Stack>
+    //   <StatusBar style="auto" />
+    // </Provider>
+    <SignupLoginScreen/>
   );
 }
